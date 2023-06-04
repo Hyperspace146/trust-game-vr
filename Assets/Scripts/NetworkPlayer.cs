9b9d9860423dd9a86957970f0e5e0fee4c2eccb7
Hyperspace146 <adriandinh146@gmail.com>
2023-06-03 19:40:59 -0700

    Added networked avatar head and hands (but doesn't work for controllers), and installed gltfast and ktx packages but didn't get controllers visualizers to work

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -1,19 +1,70 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Subsystems;
 
 public class NetworkPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    private PhotonView photonView;
+
+    private GameObject headRig;
+    //private MixedRealityPose leftRig;
+    //private MixedRealityPose rightRig;
+
+    private HandsAggregatorSubsystem handsAggregator;
+
     void Start()
     {
-        
+        handsAggregator = XRSubsystemHelpers.GetFirstRunningSubsystem<MRTKHandsAggregatorSubsystem>();
+
+        photonView = GetComponent<PhotonView>();
+        headRig = GameObject.FindWithTag("MainCamera");
+        if (photonView.IsMine)
+        {
+            foreach (var item in GetComponentsInChildren<Renderer>())
+            {
+                item.enabled = false;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (photonView.IsMine)
+        {
+            head.position = headRig.transform.position;
+            head.rotation = headRig.transform.rotation;
+            //if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Right, out rightRig))
+            //{
+            //    rightHand.transform.position = rightRig.Position;
+            //    rightHand.transform.rotation = rightRig.Rotation;
+            //}
+            //if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Palm, Handedness.Left, out leftRig))
+            //{
+            //    leftHand.transform.position = leftRig.Position;
+            //    leftHand.transform.rotation = leftRig.Rotation;
+            //}
+
+            HandJointPose leftHandPose;
+            if (handsAggregator.TryGetJoint(TrackedHandJoint.Palm, UnityEngine.XR.XRNode.LeftHand, out leftHandPose))
+            {
+                rightHand.transform.position = leftHandPose.Position;
+                rightHand.transform.rotation = leftHandPose.Rotation;
+            }
+            HandJointPose rightHandPose;
+            if (handsAggregator.TryGetJoint(TrackedHandJoint.Palm, UnityEngine.XR.XRNode.RightHand, out rightHandPose))
+            {
+                leftHand.transform.position = rightHandPose.Position;
+                leftHand.transform.rotation = rightHandPose.Rotation;
+            }
+        }
     }
 }
